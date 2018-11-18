c7433dc8be1aa1d5014cb726585d1657cae0052c
Danielle Butters <daniellebutters@Danielles-MacBook.local>
2018-11-18 16:06:40 -0500

    svg animations added

--- js/main.js
+++ js/main.js
@@ -35,3 +35,42 @@
         });
 
 })();
+
+
+
+
+
+//variables
+var lightbox = document.querySelector('.lightbox'),
+	closeLightB = document.querySelector('.closeLightbox'),
+	galleryThumb = document.querySelectorAll('.galleryThumb'),
+	mainphoto = document.querySelector('#portfolioImage'),
+	imageDesc = document.querySelector('#imageDesc'),
+	count = 0;
+
+
+
+// LIGHTBOX    
+//functions
+
+// function openLBox(){
+// 	lightbox.classList.add('show-lightbox');
+// }
+
+// function closeLbox(){
+//   lightbox.classList.remove('show-lightbox');
+// }
+
+// function showPortfolioImage(){
+// 	mainphoto.src = this.querySelector('img').src.replace('/thumbs','');
+// 	imageDesc.innerHTML = this.querySelector('.projectDesc').innerHTML;
+// 	lightbox.classList.add('show-lightbox');
+// }
+
+// //event listeners
+
+// for(var i = 0; i < galleryThumb.length; i++) {
+// 	galleryThumb[i].addEventListener('click', showPortfolioImage, false);
+// }
+
+// closeLightB.addEventListener('click', closeLbox, false);
